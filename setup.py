2632fa847d9982949ed469f12ea1ed740ce97cac
kayode-adechinan <kayode.adechinan@gmail.com>
2018-09-07 08:56:25 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniestadechinan',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/kayode-adechinan/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
